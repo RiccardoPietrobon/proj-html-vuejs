8b6f5ba15ded5925c1df7de961ffda7e4a760bc0
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-02-21 01:48:18 +0100

    campanionship-right

--- JS/app.js
+++ JS/app.js
@@ -40,6 +40,27 @@ const app = Vue.createApp({
                     numbers: [14, 76],
                 },
             ],
+
+            listBenefits: [
+                {
+                    image: "home-2-image-box-01.png",
+                    title: "Customize Preferences",
+                    text: "Learnes are given the right to arrange and customize their study schedule and timetable based on preferences.",
+                    link: "Start here",
+                },
+                {
+                    image: "about-us-02-image-box-02.png",
+                    title: "Cloud Storage",
+                    text: "You can store online lessons via online cloud or download to your device and revise lessons on the way",
+                    link: "Get free Quote",
+                },
+                {
+                    image: "home-2-image-box-03.png",
+                    title: "Multimedia Channels",
+                    text: "We provide study materials with various formats: video, audio, slides, doc, prints, books and applications.",
+                    link: "Start now",
+                },
+            ],
         }
     },
 
